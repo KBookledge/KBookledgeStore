e71614cdb64bebaeffc12f988d2b398a37655fca
myke <myke.programacao@gmail.com>
2023-01-02 18:31:06 -0300

    feat:create routes, create, update, delete, list one user, login

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from .models import User
+from .serializers import UserSerializer
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework import generics
+from .permissions import IsAccountOwnerOrSuperuser
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 
-# Create your views here.
+class UserView(generics.CreateAPIView):
+
+    queryset = User.objects.all()
+
+    serializer_class = UserSerializer
+
+class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
+
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsAccountOwnerOrSuperuser]
+
+    queryset = User.objects.all()
+
+    serializer_class = UserSerializer
