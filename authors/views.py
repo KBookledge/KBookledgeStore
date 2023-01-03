a618b166db416835f1ba911d4c8593019d04c077
ThiagoDalto <56813158+ThiagoDalto@users.noreply.github.com>
2023-01-03 00:36:40 -0300

    feat: Criação da view, que atualiza, deleta e lista

--- authors/views.py
+++ authors/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import BasePermission
+from rest_framework.pagination import PageNumberPagination
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .serializers import AuthorsSerializer
+from .models import Author
+from books.models import Book
+from .permissions import IsSuperuser
 
-# Create your views here.
+
+class AuthorsDetailView(RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsSuperuser, BasePermission]
+    serializer_class = AuthorsSerializer
+
+    queryset = Author.objects.all()
