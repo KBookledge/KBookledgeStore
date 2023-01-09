11080e2c8b504979c4c97e2b71a6b50994d94bf8
Antoniopedro2121 <salviano36789@gmail.com>
2023-01-09 08:48:37 -0300

    fix/adicionando Allowed_Host

--- _core/settings.py
+++ _core/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = getenv("SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["web-production-544c.up.railway.app", "0.0.0.0"]
 
 
 
