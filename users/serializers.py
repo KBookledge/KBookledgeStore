3bb0a8aa9461cd9e0b70228d45971bb295106cd6
myke <myke.programacao@gmail.com>
2023-01-10 15:28:39 -0300

    feat:add cpf in validations length and invalid cpf, introduction not formating

--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from .models import User
-
+from rest_framework.response import Response
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,10 +16,14 @@ class UserSerializer(serializers.ModelSerializer):
             "is_superuser",
             "registered_at",
             "updated_at",
+            "cpf"
         ]
         read_only_fields = ["registered_at", "updated_at"]
         extra_kwargs = {
             "password": {"write_only": True},
+            "cpf": {
+                "write_only": True
+            },
             "email": {
                 "write_only": True,
                 "validators": [UniqueValidator(queryset=User.objects.all())],
@@ -27,6 +31,8 @@ class UserSerializer(serializers.ModelSerializer):
         }
 
     def create(self, validated_data: dict) -> User:
+
+        print( type( validated_data["cpf"] ) )
 
         if "is_superuser" in validated_data and validated_data["is_superuser"]:
             return User.objects.create_superuser(**validated_data)
